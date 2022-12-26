ebb83f470f77732096746568612d70b22cd11601
danila <dan1981505@gmail.com>
2022-12-26 20:41:40 +0300

    added 3 controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resource :session, only: [:new, :create]
   resources :users
+
+  root "homepage#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
